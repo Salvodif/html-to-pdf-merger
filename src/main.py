ac20474e436b1379bb0767f676c294d9027de2ed
Salvodif <salvatore.difazio@gmail.com>
2025-05-11 17:08:34 +0200

    some hardcoded text removed

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ from utils.pdf_generator import generate_pdf
 
 def main():
     # Define the directory containing HTML files
-    html_directory = 'C:\My Web Sites\Carimo STh III\www.carimo.it\somma-teologica'
+    html_directory = '/path/of/htmls'
     
     # Step 1: Merge HTML files
     merged_html = merge_html_files(html_directory)
@@ -14,7 +14,7 @@ def main():
     cleaned_html = remove_background(merged_html)
     
     # Step 3: Generate PDF from the cleaned HTML
-    output_pdf_path = 'Carimo - STh II-II.pdf'
+    output_pdf_path = 'Filename.pdf'
     generate_pdf(cleaned_html, output_pdf_path)
     
     print(f"PDF generated successfully at {output_pdf_path}")
